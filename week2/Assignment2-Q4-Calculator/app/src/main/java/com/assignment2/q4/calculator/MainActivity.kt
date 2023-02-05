726fdf2538fe713e9073866c44deac788e491fc9
sarahtb25 <sbonna@bu.edu>
2023-02-05 18:32:03 -0500

    Working on calculator

--- week2/Assignment2-Q4-Calculator/app/src/main/java/com/assignment2/q4/calculator/MainActivity.kt
+++ week2/Assignment2-Q4-Calculator/app/src/main/java/com/assignment2/q4/calculator/MainActivity.kt
@@ -2,10 +2,36 @@ package com.assignment2.q4.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import com.assignment2.q4.calculator.databinding.ActivityMainBinding
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.calculateButton.setOnClickListener {
+            val operand1Input: EditText = findViewById (R.id.operand1_number)
+            Log.d(TAG, "Operand 1: ${operand1Input.text}")
+
+            val radioGroup: RadioGroup = findViewById(R.id.operations_radio_group)
+            val operation: RadioButton = findViewById(radioGroup.checkedRadioButtonId)
+            Log.d(TAG, "Operation: ${operation.text}")
+
+            val operand2Input: EditText = findViewById (R.id.operand2_number)
+            Log.d(TAG, "Operand 2: ${operand2Input.text}")
+
+            Toast.makeText(applicationContext,"On click : ${operand1Input.text} ${operation.text} ${operand2Input.text}",
+                Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
