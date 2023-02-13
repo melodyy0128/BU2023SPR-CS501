345cf0dd19b528f6d9bdfeda95a0917fb28333a6
Melody Chan <melodychan@Melodys-MacBook-Pro.local>
2023-02-12 19:07:26 -0500

    cheat activity test

--- week3/Assignment3-Q3&Q4-GeoQuiz/GeoQuiz/app/src/androidTest/java/com/bignerdranch/android/geoquiz/CheatActivityTest.kt
+++ week3/Assignment3-Q3&Q4-GeoQuiz/GeoQuiz/app/src/androidTest/java/com/bignerdranch/android/geoquiz/CheatActivityTest.kt
@@ -1,18 +1,30 @@
 package com.bignerdranch.android.geoquiz
 
+import android.support.test.uiautomator.UiDevice
 import androidx.test.core.app.ActivityScenario
-import org.junit.Assert.*
-
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.not
 import org.junit.After
+import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
+
 
 class CheatActivityTest {
 
-    private lateinit var scenario: ActivityScenario<CheatActivity>
+    private lateinit var scenario: ActivityScenario<MainActivity>
+    val device = UiDevice.getInstance(getInstrumentation())
+
 
     @Before
     fun setUp() {
-        scenario = ActivityScenario.launch(CheatActivity::class.java)
+        scenario = ActivityScenario.launch(MainActivity::class.java)
     }
 
     @After
@@ -21,6 +33,38 @@ class CheatActivityTest {
     }
 
     // rotatedStillCheating
+    @Test
+    fun rotatedStillCheating() {
+        onView(withId(R.id.cheat_button)).perform(click())
+        onView(withId(R.id.show_answer_button)).perform(click())
+        onView(withId(R.id.answer_text_view))
+            .check(matches(withText(R.string.true_button)))
+        device.setOrientationLeft()
+        onView(withId(R.id.answer_text_view))
+            .check(matches(withText(R.string.true_button)))
+        device.setOrientationNatural()
+        device.pressBack()
+        onView(withId(R.id.true_button)).perform(click())
+        onView(withText(R.string.judgment_toast))
+            .check(matches(withId(com.google.android.material.R.id.snackbar_text)))
+
+//            .inRoot(withDecorView())
+
+//        onView(withText(R.string.judgment_toast)).inRoot(
+//            withDecorView(
+//                not(
+//                    `is`(
+//                        MainActivity.getWindow().getDecorView()
+//                    )
+//                )
+//            )
+//        ).check(
+//            matches(
+//                isDisplayed()
+//            )
+//        )
+
+    }
 
     // showOnlyWhenCheated
 
